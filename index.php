99ded589e95fd93f4bbb7c86dbf543b8409464e6
ondrui <ondrui@yandex.ru>
2020-09-13 06:13:06 +0300

    загрузить

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Моя кошка</title>
+    <style>
+      p {
+        margin: 0 !important;
+        padding:0;
+      }
+      h1, .head-pet {
+        text-align: center;
+        color: steelblue;
+        margin-bottom: 20px;
+        margin-top: 20px;
+      }
+      .container-fluid {
+        border: 3px solid grey;
+      }
+      .foto {
+        width: 100%;
+      }
+      .foto-description {
+        padding: 10px;
+      }
+      .foto-description, .text-pet{
+        text-align: justify;
+        font-size: 15px;
+      }
+      .icon {
+         text-align: justify;
+        font-size: 10px;
+      }
+      .text-pet {
+        padding: 10px 30px;
+      }
+      .icon {
+        padding: 10px;
+      }
+      .sign {
+        background-color: grey;
+      }
+      .iconfoto1 {
+        float:left;
+        border-radius: 500px;
+        width: 50px;
+        height: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Моя кошка</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FB3021; font-size: 2rem">
+ <!-- <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>-->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="http://ondrui.beget.tech/my-pet" target="_blank">Главная</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="http://ondrui.beget.tech/my-pet" target="_blank">О питомце</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="http://ondrui.beget.tech/my-pet" target="_blank">Умения</a>
+      </li>
+      <!--<li class="nav-item active">
+        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>-->
+    </ul>
+  </div>
+</nav>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-7 p-0">
+      <img src="https://cdn.lifehacker.ru/wp-content/uploads/2019/11/cat_1572613841-1140x570.jpg" alt="" class="foto">
+    </div>
+    <div class="col-5">
+      <p class="foto-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, sint, veniam pariatur sed quos voluptatibus deserunt architecto deleniti vero quaerat. Recusandae, modi ab commodi consectetur quasi quibusdam inventore quae ullam?Fuga, totam excepturi est sit aut. Fugit, consequatur, tempora dolor impedit ad repellat corporis amet doloremque unde non maiores voluptate id harum optio molestias voluptas error? Odit, sit tempora fugit.
+      Ad, repellat, repellendus quod corrupti quam eveniet aut sit suscipit dolores corporis cumque beatae assumenda temporibus vero quas iure perspiciatis a dolorum illo alias consequatur saepe reiciendis libero sapiente hic!
+      Facilis, doloremque, neque, consequatur ullam tempore eligendi deleniti alias ut architecto magnam quibusdam aliquam iusto.</p>
+    </div>
+  </div>
+</div>
+<div>
+  <h2 class="head-pet">О питомце</h2>
+  <p class="text-pet">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, molestias adipisci distinctio provident dignissimos voluptates cumque aliquam sed eligendi deserunt minus voluptate suscipit illum quibusdam quasi eius vero similique nihil?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, repudiandae, facere sapiente voluptatem rerum animi consequuntur debitis dolorem adipisci ut possimus itaque ipsum excepturi commodi porro et eos nostrum quas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, ex, tempore eveniet iure corporis ducimus magnam aperiam beatae ullam asperiores minima delectus nulla optio velit maxime quae aut eligendi sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores neque dolorum earum cupiditate reprehenderit! Qui, autem, repellat, aut blanditiis accusamus laborum ipsa ut natus magnam reprehenderit architecto neque eveniet quo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, molestias adipisci distinctio provident dignissimos voluptates cumque aliquam sed eligendi deserunt minus voluptate suscipit illum quibusdam quasi eius vero similique nihil?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, repudiandae, facere sapiente voluptatem rerum animi consequuntur debitis dolorem adipisci ut possimus itaque ipsum excepturi commodi porro et eos nostrum quas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, ex, tempore eveniet iure corporis ducimus magnam aperiam beatae ullam asperiores minima delectus nulla optio velit maxime quae aut eligendi sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores neque dolorum earum cupiditate reprehenderit! Qui, autem, repellat, aut blanditiis accusamus laborum ipsa ut natus magnam reprehenderit architecto neque eveniet quo.
+  </p>
+</div>
+<div class="container-fluid icons">
+  <div class="row">
+    <div class="col-3 icon">
+      <img src="acana.jpg" alt="корм" class="iconfoto1">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, possimus, reprehenderit, ipsam sequi eum enim perspiciatis fuga dicta nihil minima natus aut atque perferendis illum similique non labore voluptatibus autem!
+      </p>
+    </div>
+    <div class="col-3 icon">
+      <img src="good.jpg" alt="корм" class="iconfoto1">
+       <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, possimus, reprehenderit, ipsam sequi eum enim perspiciatis fuga dicta nihil minima natus aut atque perferendis illum similique non labore voluptatibus autem!
+      </p>
+    </div>
+    <div class="col-3 icon">
+      <img src="play.jpg" alt="корм" class="iconfoto1">
+       <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, possimus, reprehenderit, ipsam sequi eum enim perspiciatis fuga dicta nihil minima natus aut atque perferendis illum similique non labore voluptatibus autem!
+      </p>
+    </div>
+    <div class="col-3 icon">
+      <img src="son.jpg" alt="корм" class="iconfoto1">
+       <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, possimus, reprehenderit, ipsam sequi eum enim perspiciatis fuga dicta nihil minima natus aut atque perferendis illum similique non labore voluptatibus autem!
+      </p>
+    </div>
+  </div>
+</div>
+<footer>
+  <p class="sign">© Version: 1.1 Андрей Соловьев</p>
+</footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
